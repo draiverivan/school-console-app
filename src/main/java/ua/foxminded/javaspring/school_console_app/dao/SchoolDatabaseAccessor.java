575c2001e7c35d05f4d1d5b2135d8d7fc6b86488
Ivan T <ivan.turchyn94@gmail.com>
2023-07-24 10:30:30 +0200

    Update#2 24.07

--- src/main/java/ua/foxminded/javaspring/school_console_app/dao/SchoolDatabaseAccessor.java
+++ src/main/java/ua/foxminded/javaspring/school_console_app/dao/SchoolDatabaseAccessor.java
@@ -7,9 +7,15 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SchoolDatabaseAccessor {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-	public List<Integer> getGroupIds(Connection connection) throws SQLException {
+public class SchoolDatabaseAccessor {
+	
+	private static final Logger logger = LoggerFactory.getLogger(SchoolDatabaseAccessor.class.getName());
+	  private static final String ERROR = "An error occurred: {}";
+
+	public List<Integer> getGroupIds(Connection connection) {
 		List<Integer> ids = new ArrayList<>();
 
 		String sql = "SELECT group_id FROM school.groups";
@@ -19,11 +25,13 @@ public class SchoolDatabaseAccessor {
 			while (resultSet.next()) {
 				ids.add(resultSet.getInt("group_id"));
 			}
-		}
+		} catch (SQLException e) {
+	        logger.error(ERROR, e.getMessage(), e);
+	      }
 		return ids;
 	}
 
-	public List<Integer> getStudentIds(Connection connection) throws SQLException {
+	public List<Integer> getStudentIds(Connection connection) {
 		List<Integer> ids = new ArrayList<>();
 
 		String sql = "SELECT student_id FROM school.students";
@@ -33,11 +41,13 @@ public class SchoolDatabaseAccessor {
 			while (resultSet.next()) {
 				ids.add(resultSet.getInt("student_id"));
 			}
-		}
+		} catch (SQLException e) {
+	        logger.error(ERROR, e.getMessage(), e);
+	      }
 		return ids;
 	}
 
-	public List<Integer> getCourseIds(Connection connection) throws SQLException {
+	public List<Integer> getCourseIds(Connection connection) {
 		List<Integer> ids = new ArrayList<>();
 
 		String sql = "SELECT course_id FROM school.courses";
@@ -47,7 +57,9 @@ public class SchoolDatabaseAccessor {
 			while (resultSet.next()) {
 				ids.add(resultSet.getInt("course_id"));
 			}
-		}
+		} catch (SQLException e) {
+	        logger.error(ERROR, e.getMessage(), e);
+	      }
 		return ids;
 	}
 }
